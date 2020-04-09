ead686621c03ee11e4fcb7f3650fed45a64a6081
ermaozi <747761541@qq.com>
2020-04-09 09:54:47 +0800

    加歌

---
+++ get_music.py
@@ -0,0 +1,7 @@
+import os
+import re
+songs = os.listdir("./static/music_data/songs")
+with open("./static/js/player.js", "r", encoding="utf-8") as f:
+    content = f.read()
+with open("./static/js/player.js", "w", encoding="utf-8") as f:
+    f.write(re.sub("music_data=(\[[\s\S]+?\])", f"music_data={str(songs)}", content))
