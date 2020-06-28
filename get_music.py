84bcaf655c56de8894062973cb7a07d1d4cfc2fb
二猫子 <747761541@qq.com>
2020-06-28 10:04:31 +0800

    Update get_music.py

--- get_music.py
+++ get_music.py
@@ -1,6 +1,7 @@
 import os
 import re
 songs = os.listdir("./static/music_data/songs")
+print(songs)
 with open("./static/js/player.js", "r", encoding="utf-8") as f:
     content = f.read()
 with open("./static/js/player.js", "w", encoding="utf-8") as f:
